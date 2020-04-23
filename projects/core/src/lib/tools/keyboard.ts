66bbd5ea061f8ee2145a85ef463c386dd20e05f2
claytoncc <clayton@openiot.co.za>
2020-04-23 18:46:06 +0200

    updates

--- projects/core/src/lib/tools/keyboard.ts
+++ projects/core/src/lib/tools/keyboard.ts
@@ -2,12 +2,26 @@ import { Subject } from 'rxjs';
 
 export class KeyboardTool {
 
+    public alt:     boolean = false;
+    public ctrl:    boolean = false;
+    public shift:   boolean = false;
+
     public keyup:   Subject<KeyboardEvent> = new Subject<KeyboardEvent>();
     public keydown: Subject<KeyboardEvent> = new Subject<KeyboardEvent>();
     
     constructor() {
-        document.addEventListener('keyup', (event) => this.keyup.next(event));
-        document.addEventListener('keydown', (event) => this.keydown.next(event));
+        document.addEventListener('keyup', (event: KeyboardEvent) => {
+            this.alt    = event.altKey;
+            this.ctrl   = event.ctrlKey;
+            this.shift  = event.shiftKey;
+            this.keyup.next(event);
+        });
+        document.addEventListener('keydown', (event: KeyboardEvent) => {
+            this.alt    = event.altKey;
+            this.ctrl   = event.ctrlKey;
+            this.shift  = event.shiftKey;
+            this.keydown.next(event);
+        });
     };
 
 }
