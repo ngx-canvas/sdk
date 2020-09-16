777cfa0d430985dd10ceeb5d67faa3a3579ed2e4
claytoncc <clayton@openiot.co.za>
2020-09-16 11:03:12 +0200

    updates

--- projects/core/src/lib/utilities/series.ts
+++ projects/core/src/lib/utilities/series.ts
@@ -1,10 +1,13 @@
+import { ObjectId } from '../id';
 import { FILL, Fill } from './fill';
 import { STROKE, Stroke } from './stroke';
 
 export class Series {
 
+    public id: string = ObjectId();
     public type: string;
     public fill: FILL = new Fill();
+    public data: any = {};
     public title: string = '';
     public value: number[] = [];
     public stroke: STROKE = new Stroke();
@@ -14,11 +17,17 @@ export class Series {
             if (Array.isArray(series.value)) {
                 this.value = series.value;
             };
+            if (typeof (series.id) != 'string') {
+                this.id = series.id;
+            };
             if (typeof (series.type) == 'string') {
                 this.type = series.type;
             };
             if (typeof (series.title) == 'string') {
                 this.title = series.title;
+            };
+            if (typeof (series.data) != 'undefined' && series.data != null) {
+                this.data = series.data;
             };
             if (typeof (series.fill) != 'undefined' && series.fill != null) {
                 this.fill = new Fill(series.fill);
@@ -32,6 +41,8 @@ export class Series {
 }
 
 export interface SERIES {
+    'id'?: string;
+    'data'?: any;
     'type'?: string;
     'fill'?: FILL;
     'title'?: string;
