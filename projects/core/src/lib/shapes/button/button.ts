d5f841f418593c383a70c50c8eab66a457507b19
claytoncc <clayton.constant@gmail.com>
2023-02-03 22:01:36 +0000

    Update button

--- projects/core/src/lib/shapes/button/button.ts
+++ projects/core/src/lib/shapes/button/button.ts
@@ -15,20 +15,36 @@ export class Button extends Shape {
   };
 
   apply(parent: any) {
-    this.el = parent.append('rect')
+    function color(hex: string, opacity: number) {
+      let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+      return result ? `rgba(${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)}, ${opacity / 100}` : null;
+    }
+    this.el = parent.append('foreignObject')
       .attr('x', !(this.stroke.width % 2) ? this.position.x : this.position.x + 0.5)
       .attr('y', !(this.stroke.width % 2) ? this.position.y : this.position.y + 0.5)
       .attr('id', this.id)
-      .attr('rx', this.position.radius)
-      .attr('fill', this.fill.color)
+      .attr('name', this.name)
       .attr('width', this.position.width)
-      .attr('stroke', this.stroke.color)
       .attr('height', this.position.height)
-      .attr('fill-opacity', this.fill.opacity)
-      .attr('stroke-width', this.stroke.width)
-      .attr('stroke-linecap', this.stroke.cap)
-      .attr('stroke-opacity', this.stroke.opacity)
-    // .attr('stroke-dasharray', this.stroke.style)
+      .append('xhtml:button')
+      .style('color', color(this.font.color, this.font.opacity))
+      .style('width', this.position.width + 'px')
+      .style('height', this.position.height + 'px')
+      .style('display', 'flex')
+      .style('font-size', this.font.size + 'px')
+      .style('font-style', this.font.style.includes('italic') ? 'italic' : null)
+      .style('font-family', this.font.family)
+      .style('font-weight', this.font.style.includes('bold') ? 'bold' : 'normal')
+      .style('align-items', this.font.alignment)
+      .style('border-width', this.stroke.width + 'px')
+      .style('border-color', color(this.stroke.color, this.stroke.opacity))
+      .style('border-style', this.stroke.style)
+      .style('border-radius', this.position.radius + 'px')
+      .style('stroke-linecap', this.stroke.cap)
+      .style('text-decoration', this.font.style.includes('underline') ? 'underline' : null)
+      .style('justify-content', this.font.baseline)
+      .style('background-color', color(this.fill.color, this.fill.opacity))
+      .html(this.value)
   }
 
 }
