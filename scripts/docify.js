2a7213d0cc3263b684e9727b3aa8a21df97ff7cd
claytoncc <clayton.constant@gmail.comil.com>
2023-12-23 16:14:25 +0000

    Updates

--- scripts/docify.js
+++ scripts/docify.js
@@ -70,5 +70,5 @@ const docs = projects.map(project => {
   }
 })
 
-console.log('Writing file into assets!', path.join(__dirname, '../dist/documentation.json'))
-fs.writeFileSync(path.join(__dirname, '../dist/documentation.json'), JSON.stringify(docs, null, 2))
+console.log('Writing file into assets!', path.join(__dirname, '../dist/data.json'))
+fs.writeFileSync(path.join(__dirname, '../dist/data.json'), JSON.stringify(docs, null, 2))
